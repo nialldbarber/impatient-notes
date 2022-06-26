cefd1b6e2f03bbe08bec925c167775c428dce1a7
Niall Barber <niall.barber@olioex.com>
2022-06-26 10:22:19 +0100

    Updated prettier config
    Added map

--- arrays.ts
+++ arrays.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.145.0/testing/asserts.ts"
 
 /**
-  Use Array.prototype.flatMap() to do so.
+  Use Array.prototype.flatMap()
   Use Number() to convert arbitrary values to numbers.
 */
 const convertToNumbers = (arr: any[]) =>
@@ -10,15 +10,24 @@ const convertToNumbers = (arr: any[]) =>
     .filter((item) => !isNaN(item))
 
 Deno.test("convertToNumbers() via flatMap()", () => {
-  assertEquals(convertToNumbers(["", "1", "2", "abc", "3"]), [0, 1, 2, 3])
+  assertEquals(
+    convertToNumbers(["", "1", "2", "abc", "3"]),
+    [0, 1, 2, 3]
+  )
   assertEquals(convertToNumbers([4, 5, NaN, 6, NaN, 7]), [4, 5, 6, 7])
-  assertEquals(convertToNumbers([false, true, null, undefined]), [0, 1, 0])
+  assertEquals(
+    convertToNumbers([false, true, null, undefined]),
+    [0, 1, 0]
+  )
 })
 
 /**
   Use Array.prototype.reduce()
 */
-const countMatches = (arr: any[], callbackFn: (num: number) => boolean) =>
+const countMatches = (
+  arr: any[],
+  callbackFn: (num: any) => boolean
+) =>
   arr.reduce(
     (previousValue, currentValue) =>
       callbackFn(currentValue) ? previousValue + 1 : previousValue,
@@ -44,12 +53,10 @@ Deno.test("countMatches() via .reduce()", () => {
   )
 
   assertEquals(
-    // @ts-ignore
     countMatches(["a", "", "b"], (x) => x.length > 0),
     2
   )
   assertEquals(
-    // @ts-ignore
     countMatches(["a", "", "b"], (x) => x.length === 0),
     1
   )
@@ -57,11 +64,11 @@ Deno.test("countMatches() via .reduce()", () => {
 
 /**
   Use Array.prototype.reduce()
-  You can mutate (change) the current state in the callback 
-  (but you can also solve this exercise without doing so)
 */
-
-const filter = (arr: any[], callback: (...args: any[]) => boolean) => {
+const filter = (
+  arr: any[],
+  callback: (...args: any[]) => boolean
+) => {
   return arr.reduce((result, curr, index) => {
     if (callback(curr, index, arr)) {
       result.push(curr)
@@ -94,5 +101,41 @@ Deno.test("filter", () => {
   assertEquals(
     filter(["a", "b", "c", "d"], (x, i) => i % 2 === 0),
     ["a", "c"]
+  )
+})
+
+/*
+- Use Array.prototype.reduce() to implement map_via_reduce.mjs
+*/
+const map = (
+  arr: any[],
+  callback: (...args: any[]) => number | string
+) => {
+  return arr.reduce(
+    (prev, curr, index) => [...prev, callback(curr, index)],
+    []
+  )
+}
+
+Deno.test("map() via .reduce()", () => {
+  assertEquals(
+    map([1, 2, 3], (x) => x * 2),
+    [2, 4, 6]
+  )
+  assertEquals(
+    map([1, 2, 3], (x) => "a"),
+    ["a", "a", "a"]
+  )
+  assertEquals(
+    map(["a", "b", "c"], (x, i) => i),
+    [0, 1, 2]
+  )
+  assertEquals(
+    map(["a", "b", "c"], (x) => x),
+    ["a", "b", "c"]
+  )
+  assertEquals(
+    map([], (x) => x),
+    []
   )
 })
